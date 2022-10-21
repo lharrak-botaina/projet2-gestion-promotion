dfcda88cafdd468b3d6b53bc3e62d73c4835946c
lharrak-botaina <lharrak.botaina@gmail.com>
2022-10-21 10:18:07 +0100

    [ADD]edit& delete action

---
+++ prototype-standard/resources/views/index.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div class="col-sm-12">
+
+        @if(session()->get('success'))
+          <div class="alert alert-success">
+            {{ session()->get('success') }}
+          </div>
+        @endif
+      </div>
+    <a href="{{route('promotion.create')}}">ajouter </a>
+    <table>
+        <thead>
+            <tr>
+                <th>Nom</th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse ($promotion as $value)
+            <tr>
+                <td >{{$value ->name}}</td>
+                <td><a href="{{ route('promotion.edit', $value->id)}}" >Modifier</a></td>
+                <td>
+                    <form action="{{ route('promotion.destroy', $value->id)}}" method="post">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit">supprimer</button>
+                      
+                      </form>
+                </td>
+
+            </tr>
+            @empty
+
+            @endforelse
+        </tbody>
+    </table>
+
+</body>
+</html>
+
